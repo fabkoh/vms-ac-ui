02a77dc8fd434cb92d7f494b3501d98296062459
Fabian Koh <koh.fabian@gmail.com>
2021-06-29 21:26:49 +0800

    Added Register component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
+import Button from 'react-bootstrap/Button';
+import { Container, Row, Col } from 'react-bootstrap';
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
+import Register from './Register';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App-header">
+          <Container align="center">
+            <Row>
+              <Col><Header /></Col>
+            </Row>
+            <Row>
+              <Col><br></br></Col>
+            </Row>
+            <Row>
+              <Col><Link to="/register">Register a visitor</Link></Col>
+            </Row>
+        </Container>
+      <Switch>
+        <Route path="/register"><Register/></Route>
+      </Switch>
+      </div>
+    </Router>
   );
 }
 
