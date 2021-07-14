1299015acaeb6aa97e2df3603489b642e4da0eee
Fabian Koh <koh.fabian@gmail.com>
2021-07-14 19:43:14 +0800

    Modified QrCodeDisplay component

--- src/components/QrCodeDisplay.js
+++ src/components/QrCodeDisplay.js
@@ -1,29 +1,43 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const QrCodeDisplay = () => {
-
+  
     const urlParams = new URLSearchParams(window.location.search);
     const qrId = urlParams.get('qrCode');
     const fetchQrURL = 'http://localhost:8080/api/qr-code/'+qrId;
+    const [visitDetails, setVisitDetails] = useState([]);
+    const fetchVisitURL = 'http://localhost:8080/api/visit-by-qrcodeid/'+qrId;
 
-    console.log(fetchQrURL);
     useEffect(()=> {
 
-        const getQrCode = async (fetchQrURL) => {
-            const res = await fetch(fetchQrURL,{
-                method: 'GET',
-                headers: {
-                    'Content-type': 'image/jpeg',
-                }
-            });
+       
+        console.log(fetchVisitURL);
+        
+        const fetchVisitDetail = async () => {
+            try{
+                const res = await fetch(fetchVisitURL,{
+                    method: 'GET',
+                    headers: {
+                        'Content-type': 'application/json',
+                    },
+                });
+                const jsonResponse = res.json();
+                console.log(jsonResponse);
+                setVisitDetails(jsonResponse);
+                
             
+            }catch(error){
+                console.log("error", error);
+            }
         };
-        getQrCode();
-    },[]);
+        fetchVisitDetail();
+        
+    },[fetchVisitURL]);
 
     return (
         <div>
             <h2>Your QR entry pass</h2>
+            <h3>Start Date of Visit:</h3>
             <img src={fetchQrURL} alt="QR entry" width="200px" height="200px"></img>
         </div>
     )
